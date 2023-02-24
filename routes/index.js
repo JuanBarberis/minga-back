5931c334080fc5d634b744a9e9fd345d277b6e9d
JuanBarberis <juanbarberis.97@gmail.com>
2023-02-24 12:14:11 -0300

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 
 import express from 'express'
+import usersRouter from './users.js'
 let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sprint 1' });
 });
+app.use('/users', usersRouter);
 
 export default router
